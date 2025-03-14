580d01457909a00b47f226e79cfeb836f7c00368
prototype956 <yyz70758527@163.com>
2025-03-14 20:53:06 +0800

    修改博客标题

--- yukina.config.ts
+++ yukina.config.ts
@@ -2,9 +2,9 @@ import I18nKeys from "./src/locales/keys";
 import type { Configuration } from "./src/types/config";
 
 const YukinaConfig: Configuration = {
-  title: "Yukina",
-  subTitle: "Yukina Template Demo Site",
-  brandTitle: "Yukina",
+  title: "Prototype",
+  subTitle: "进行中：",
+  brandTitle: "prototype",
 
   description: "Demo Site",
 
